75757b1bdc386781540fb0fc888b1de6d2494fe4
kasumipyon <129807419+kasumipyon@users.noreply.github.com>
2023-04-25 14:26:19 +0900

    Add files via upload

--- panoramaview.js
+++ panoramaview.js
@@ -105,7 +105,7 @@ function setImgEvent(img) {
 
     ['mousedown', 'touchstart'].forEach(function (item) {
         img.addEventListener(item, function (e) {
-            e.preventDefault();
+            //e.preventDefault();
             if (e.type === "mousedown") {
                 var event = e;
             } else {
@@ -119,7 +119,6 @@ function setImgEvent(img) {
 
     ['mouseup', 'mouseleave', 'touchend', 'touchleave'].forEach(function (item) {
         img.addEventListener(item, function (e) {
-            e.preventDefault();
             let drag = document.querySelector('.drag')
             if (drag != null) {
                 drag.classList.remove('drag');
